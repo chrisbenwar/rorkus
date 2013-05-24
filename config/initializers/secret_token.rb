1d48fd60a923e58a5e920944ed4036344d1bd2bb
Chris <chris@chris-1015PX.(none)>
2013-05-24 13:31:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorkus::Application.config.secret_token = 'bb98b77e9744c1e58d36f4c5c6534acb473c978019e1fa310f46269726f6f4ed9798786d0d33e56fc4d812ac067097d49a68e6dbdaaccb6cfc3194caa09865f0'
