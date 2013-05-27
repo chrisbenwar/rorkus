f84fd617b8ad01cc6dd93097d0240d8a2b1a9668
Chris Benwar <chris@not.email>
2013-05-27 15:32:46 +0100

    Add lots of new layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rorkus::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  get "users/new"
 
-  get "static_pages/about"
+  root :to => 'static_pages#home'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+  match '/signup', to: 'users#new'
 
   resources :microposts
   resources :users
@@ -58,7 +63,7 @@ Rorkus::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,31 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  subject { page }
 	
   let(:base_title) { "Rorkus - Ruby on Rails kus you kan" }
 
   describe "Home Page" do
+    before { visit root_path }
+    it { should have_selector('h1', text: "Rorkus") }
+    it { should have_selector('title', text: "#{base_title}")}
+  end
 
-    it "should have the content 'Rorkus'" do
-	    visit '/static_pages/home'
-	    page.should have_selector('h1', :text => 'Rorkus')
-    end
+  describe "Help Page" do
+    before { visit help_path }
+	  it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: "#{base_title} | Help") }
+  end
 
-    it "should have the right title" do
-        visit '/static_pages/home'
-        page.should have_selector('title',
-            :text => "#{base_title} | Home")
-    end
+  describe "About Page" do
+    before { visit about_path }
+	  it { should have_selector('h1', text: 'About Rorkus') }
+    it { should have_selector('title', text: "#{base_title} | About") }
+  end
 
-    it "should have the content 'Help'" do
-	    visit '/static_pages/help'
-	    page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the right title" do
-        visit '/static_pages/help'
-        page.should have_selector('title',
-            :text => "#{base_title} | Help")
-    end
-
-    it "should have the content 'About Rorkus'" do
-	    visit '/static_pages/about'
-	    page.should have_selector('h1', :text => 'About Rorkus')
-    end
-
-    it "should have the right title" do
-        visit '/static_pages/about'
-        page.should have_selector('title',
-            :text => "#{base_title} | About")
-    end
-
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: "#{base_title} | Contact") }
   end
 end
